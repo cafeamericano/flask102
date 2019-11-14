f72919e1c3860100d2f42b9ce8e44dde035bafbd
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-13 22:19:26 -0500

    Added comments to app.py

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
+# Imports
 from flask import Flask, request, jsonify
 import pymongo
 import os
 import json
 
+# Define app variable
 app = Flask(__name__)
 
+# Define database
 myclient = pymongo.MongoClient("mongodb://localhost:27017")
+database = myclient["flask102"]
+collection = database["wines"]
 
-mydb = myclient["flask102"]
-mycoll = mydb["wines"]
-
+# Routing
 @app.route('/', methods=['GET'])
 def welcome():
     return "<h1>Hello World</h1><p>Sent to you by a server running Flask.</p>"
 
-@app.route('/findone', methods=['GET'])
-def test_json():
-    query = { "boldness": "heavy" }
-    data 
-    print(data)
-    return json.dumps(data, default=str)
-
 @app.route('/findmany', methods=['GET'])
 def test_json2():
-    data = mycoll.find().sort('name')
+    data = collection.find().sort('name')
     print(data)
     arr = []
     for item in data:
@@ -33,32 +29,33 @@ def test_json2():
 @app.route('/insert', methods=['GET'])
 def addRecord():
     mydict = { "name": "Zinfandel", "boldness": "medium" }
-    x = mycoll.insert_one(mydict)
+    x = collection.insert_one(mydict)
     return "<p>Record added.</p>"
 
 @app.route('/delete', methods=['GET'])
 def deleteRecord():
     myquery = { "name": "Malbec" }
-    mycoll.delete_one(myquery)
+    collection.delete_one(myquery)
     return "<p>Record deleted.</p>"
 
 @app.route('/update', methods=['GET'])
 def updateRecord():
   myquery = { "name": "Cabernet Sauvignon" }
   newvalues = { "$set": { "name": "Cabernet" } }
-  mycoll.update_one(myquery, newvalues)
+  collection.update_one(myquery, newvalues)
   return "<p>Record updated.</p>"
 
 @app.route('/query', methods=['GET'])
 def test_json3():
     myquery = { "boldness": "heavy" }
-    data = mycoll.find(myquery)
+    data = collection.find(myquery)
     print(data)
     arr = []
     for item in data:
       arr.append(item)
     return json.dumps(arr, default=str)
 
+# Start server listening
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
